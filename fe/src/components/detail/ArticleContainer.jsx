8bc59dd0eef0719769f3630c3e257d54c7437880
yjp8842 <yjp20183029@gmail.com>
2024-08-07 14:22:57 +0900

    feat: 관련기사 페이지 구성

---
+++ fe/src/components/detail/ArticleContainer.jsx
@@ -0,0 +1,63 @@
+export default function ArticleContainer() {
+  const articles = [
+    {
+      title: "탄소배출권 급등, ETF도 날았다",
+      content:
+        "28일 한국거래소에 따르면 최근 열흘(17~27일) 사이에 ‘SOL 글로벌탄소배출권선물ICE(합성)’ ETF가 10.44% 급등했다. 이 상품은 유럽과 영국, 북미 등의 탄소배출권에 분산 투자한다. 탄소배출권은 온실가스를 배출할 수 있는 권리다. 통상 국제 유가, 천연가스 가격이 오르면 대체재인 석탄 수요가 많아지면서 배출권 가격이 비싸지는 경향이 있다. 같은 기간 ‘SOL 유럽탄소배출권선물S&P(H)’ ‘KODEX 유럽탄소배출권선물ICE(H)’ ‘HANARO 글로벌탄소배출권선물ICE(합성)’도 10% 이상 상승했다.",
+      image_url: "https://img.hankyung.com/photo/202405/AA.36859536.1.jpg",
+    },
+    {
+      title: "탄소배출권 급등, ETF도 날았다",
+      content:
+        "28일 한국거래소에 따르면 최근 열흘(17~27일) 사이에 ‘SOL 글로벌탄소배출권선물ICE(합성)’ ETF가 10.44% 급등했다. 이 상품은 유럽과 영국, 북미 등의 탄소배출권에 분산 투자한다. 탄소배출권은 온실가스를 배출할 수 있는 권리다. 통상 국제 유가, 천연가스 가격이 오르면 대체재인 석탄 수요가 많아지면서 배출권 가격이 비싸지는 경향이 있다. 같은 기간 ‘SOL 유럽탄소배출권선물S&P(H)’ ‘KODEX 유럽탄소배출권선물ICE(H)’ ‘HANARO 글로벌탄소배출권선물ICE(합성)’도 10% 이상 상승했다.",
+      image_url: "https://img.hankyung.com/photo/202405/AA.36859536.1.jpg",
+    },
+    {
+      title: "탄소배출권 급등, ETF도 날았다",
+      content:
+        "28일 한국거래소에 따르면 최근 열흘(17~27일) 사이에 ‘SOL 글로벌탄소배출권선물ICE(합성)’ ETF가 10.44% 급등했다. 이 상품은 유럽과 영국, 북미 등의 탄소배출권에 분산 투자한다. 탄소배출권은 온실가스를 배출할 수 있는 권리다. 통상 국제 유가, 천연가스 가격이 오르면 대체재인 석탄 수요가 많아지면서 배출권 가격이 비싸지는 경향이 있다. 같은 기간 ‘SOL 유럽탄소배출권선물S&P(H)’ ‘KODEX 유럽탄소배출권선물ICE(H)’ ‘HANARO 글로벌탄소배출권선물ICE(합성)’도 10% 이상 상승했다.",
+      image_url: "https://img.hankyung.com/photo/202405/AA.36859536.1.jpg",
+    },
+    {
+      title: "탄소배출권 급등, ETF도 날았다",
+      content:
+        "28일 한국거래소에 따르면 최근 열흘(17~27일) 사이에 ‘SOL 글로벌탄소배출권선물ICE(합성)’ ETF가 10.44% 급등했다. 이 상품은 유럽과 영국, 북미 등의 탄소배출권에 분산 투자한다. 탄소배출권은 온실가스를 배출할 수 있는 권리다. 통상 국제 유가, 천연가스 가격이 오르면 대체재인 석탄 수요가 많아지면서 배출권 가격이 비싸지는 경향이 있다. 같은 기간 ‘SOL 유럽탄소배출권선물S&P(H)’ ‘KODEX 유럽탄소배출권선물ICE(H)’ ‘HANARO 글로벌탄소배출권선물ICE(합성)’도 10% 이상 상승했다.",
+      image_url: "https://img.hankyung.com/photo/202405/AA.36859536.1.jpg",
+    },
+    {
+      title: "탄소배출권 급등, ETF도 날았다",
+      content:
+        "28일 한국거래소에 따르면 최근 열흘(17~27일) 사이에 ‘SOL 글로벌탄소배출권선물ICE(합성)’ ETF가 10.44% 급등했다. 이 상품은 유럽과 영국, 북미 등의 탄소배출권에 분산 투자한다. 탄소배출권은 온실가스를 배출할 수 있는 권리다. 통상 국제 유가, 천연가스 가격이 오르면 대체재인 석탄 수요가 많아지면서 배출권 가격이 비싸지는 경향이 있다. 같은 기간 ‘SOL 유럽탄소배출권선물S&P(H)’ ‘KODEX 유럽탄소배출권선물ICE(H)’ ‘HANARO 글로벌탄소배출권선물ICE(합성)’도 10% 이상 상승했다.",
+      image_url: "https://img.hankyung.com/photo/202405/AA.36859536.1.jpg",
+    },
+    {
+      title: "탄소배출권 급등, ETF도 날았다",
+      content:
+        "28일 한국거래소에 따르면 최근 열흘(17~27일) 사이에 ‘SOL 글로벌탄소배출권선물ICE(합성)’ ETF가 10.44% 급등했다. 이 상품은 유럽과 영국, 북미 등의 탄소배출권에 분산 투자한다. 탄소배출권은 온실가스를 배출할 수 있는 권리다. 통상 국제 유가, 천연가스 가격이 오르면 대체재인 석탄 수요가 많아지면서 배출권 가격이 비싸지는 경향이 있다. 같은 기간 ‘SOL 유럽탄소배출권선물S&P(H)’ ‘KODEX 유럽탄소배출권선물ICE(H)’ ‘HANARO 글로벌탄소배출권선물ICE(합성)’도 10% 이상 상승했다.",
+      image_url: "https://img.hankyung.com/photo/202405/AA.36859536.1.jpg",
+    },
+    {
+      title: "탄소배출권 급등, ETF도 날았다",
+      content:
+        "28일 한국거래소에 따르면 최근 열흘(17~27일) 사이에 ‘SOL 글로벌탄소배출권선물ICE(합성)’ ETF가 10.44% 급등했다. 이 상품은 유럽과 영국, 북미 등의 탄소배출권에 분산 투자한다. 탄소배출권은 온실가스를 배출할 수 있는 권리다. 통상 국제 유가, 천연가스 가격이 오르면 대체재인 석탄 수요가 많아지면서 배출권 가격이 비싸지는 경향이 있다. 같은 기간 ‘SOL 유럽탄소배출권선물S&P(H)’ ‘KODEX 유럽탄소배출권선물ICE(H)’ ‘HANARO 글로벌탄소배출권선물ICE(합성)’도 10% 이상 상승했다.",
+      image_url: "https://img.hankyung.com/photo/202405/AA.36859536.1.jpg",
+    },
+  ];
+
+  return (
+    <div className="h-[calc(100vh_-_222px)] flex flex-col gap-8 bg-white px-[1.5rem] pt-[0.8rem] pb-[1.8rem] overflow-scroll">
+      {articles.map((article, idx) => (
+        <div key={idx} className="flex gap-4 items-center">
+          <div className="flex flex-col gap-2 text-black">
+            <span className="font-bold">{article.title}</span>
+            <span className="text-xs line-clamp-3">{article.content}</span>
+          </div>
+          <img
+            src={article.image_url}
+            className="h-[70px] min-w-[65px] rounded"
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
