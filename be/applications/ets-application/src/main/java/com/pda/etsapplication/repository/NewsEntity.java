b25c904bf06cc65da71d4a49dab1eff3f7958cb7
Dongwon KIm <dongwon000103@gmail.com>
2024-08-07 15:14:33 +0900

    feat: news

---
+++ be/applications/ets-application/src/main/java/com/pda/etsapplication/repository/NewsEntity.java
@@ -0,0 +1,36 @@
+package com.pda.etsapplication.repository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "News")
+public class NewsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "thumbnail", nullable = false)
+    private String thumbnail;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+}
--- be/applications/ets-application/src/main/java/com/pda/etsapplication/repository/StocksRepository.java
+++ be/applications/ets-application/src/main/java/com/pda/etsapplication/repository/StocksRepository.java
@@ -4,9 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface StocksRepository extends JpaRepository<StocksEntity, String> {
     List<StocksEntity> findByCountryAndSector(Integer country, String sector);
+    boolean existsByStockCode(String stockCode);
 }
