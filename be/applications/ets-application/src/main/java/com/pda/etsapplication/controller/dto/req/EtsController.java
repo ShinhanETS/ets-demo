a7e945341e91123cbdf4e2bcbc65a4b59939c294
Dongwon KIm <dongwon000103@gmail.com>
2024-08-07 15:14:33 +0900

    feat: news

--- be/applications/ets-application/src/main/java/com/pda/etsapplication/controller/dto/req/EtsController.java
+++ be/applications/ets-application/src/main/java/com/pda/etsapplication/controller/dto/req/EtsController.java
@@ -2,12 +2,14 @@ package com.pda.etsapplication.controller.dto.req;
 
 import com.pda.apiutil.ApiUtil;
 import com.pda.apiutil.GlobalResponse;
+import com.pda.etsapplication.repository.NewsEntity;
 import com.pda.etsapplication.repository.PricesEntity;
 import com.pda.etsapplication.repository.StocksEntity;
 import com.pda.etsapplication.service.EtsService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -31,6 +33,11 @@ public class EtsController {
 //            return ApiUtil.exception("데이터가 없습니다."); // 데이터가 없을 경우 404 응답
 //        }
         return ApiUtil.success("종목의 가격 데이터", prices); // 데이터가 있을 경우 200 응답과 데이터 반환
+    }
+
+    @GetMapping("/stock/{stockCode}/news")
+    public GlobalResponse<List<NewsEntity>> getNewsByStockCode(@PathVariable String stockCode) {
+        return ApiUtil.success("뉴스 가져오기 성공", etsService.getNewsByStockCode(stockCode));
     }
 }
 
