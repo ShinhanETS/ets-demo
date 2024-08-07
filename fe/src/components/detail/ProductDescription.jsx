6eb1e7facf1ec5aa10ecf6cec515b2caddccc95e
yjp8842 <yjp20183029@gmail.com>
2024-08-07 14:21:49 +0900

    feat: 상품개요 페이지 구성

---
+++ fe/src/components/detail/ProductDescription.jsx
@@ -0,0 +1,10 @@
+import Image from "../../assets/image.png";
+
+export default function ProductDescription() {
+  return (
+    <div className="h-[calc(100vh_-_222px)] flex flex-col px-[1.5rem] py-[1rem] gap-4 bg-white text-[#4E4E4E]">
+      <img src={Image} className="h-[11rem] rounded-xl bg-cover" />
+      <span>설명 어쩌구...</span>
+    </div>
+  );
+}
