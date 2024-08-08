3029a8017dcd1e7c8883c9690b9a291a42afc459
yjp8842 <yjp20183029@gmail.com>
2024-08-08 21:18:30 +0900

    feat: api 설정 추가

--- fe/src/apis/BaseApi.jsx
+++ fe/src/apis/BaseApi.jsx
@@ -1,10 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const REAL_URL = "/real";
+export const TEST_URL = "/test";
 
 // 로그인 인스턴스
 export const loginInstance = axios.create({
   baseURL: BASE_URL,
+  withCredentials: true,
+});
+
+// ets 인스턴스
+export const etsInstance = axios.create({
+  baseURL: REAL_URL,
+  withCredentials: true,
+});
+
+// ets 매수/매도 임시 인스턴스
+export const testInstance = axios.create({
+  baseURL: TEST_URL,
   withCredentials: true,
 });
 
@@ -17,9 +31,23 @@ export const membershipInstance = axios.create({
 // 인터셉터로 토큰 넣어주기 (로그인 제외 다 해줘야함)
 membershipInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 인터셉터로 토큰 넣어주기 (로그인 제외 다 해줘야함)
+testInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
     return config;
   },
--- fe/vite.config.js
+++ fe/vite.config.js
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: "http://localhost",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost",
+          changeOrigin: true,
+        },
+        "/real": {
+          target: env.VITE_REAL_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/real/, ""),
+        },
+        "/test": {
+          target: env.VITE_TEST_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/test/, ""),
+        },
       },
     },
-  },
+  };
 });
