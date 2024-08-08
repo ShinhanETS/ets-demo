985ab1ce1bc6d45db9346ed739a5c46d9c38e66d
Dongwon KIm <dongwon000103@gmail.com>
2024-08-09 01:30:04 +0900

    feat: ets description

--- be/applications/ets-application/src/main/java/com/pda/etsapplication/controller/EtsController.java
+++ be/applications/ets-application/src/main/java/com/pda/etsapplication/controller/EtsController.java
@@ -5,6 +5,7 @@ import com.pda.apiutil.GlobalResponse;
 import com.pda.etsapplication.controller.dto.res.StocksDto;
 import com.pda.etsapplication.dto.OfferReqDto;
 import com.pda.etsapplication.dto.OfferTradeResDto;
+import com.pda.etsapplication.repository.DescriptionEntity;
 import com.pda.etsapplication.repository.NewsEntity;
 import com.pda.etsapplication.repository.PricesEntity;
 import com.pda.etsapplication.service.EtsService;
@@ -67,6 +68,11 @@ public class EtsController {
     @GetMapping("/stock/{stockCode}/news")
     public GlobalResponse<List<NewsEntity>> getNewsByStockCode(@PathVariable String stockCode) {
         return ApiUtil.success("뉴스 가져오기 성공", etsService.getNewsByStockCode(stockCode));
+    }
+
+    @GetMapping("/stock/{stockCode}/desc")
+    public GlobalResponse<DescriptionEntity> getDescription(@PathVariable String stockCode) {
+        return ApiUtil.success("상품 설명 가져오기 성공", etsService.getDescription(stockCode));
     }
 }
 
