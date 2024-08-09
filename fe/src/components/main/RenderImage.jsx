21e0e6dc6ba451666ff290f911818682e6185f01
김영석 <dimondkin@nate.com>
2024-08-09 10:22:41 +0900

    feat: 기업 로고 이미지 추가

--- fe/src/components/main/RenderImage.jsx
+++ fe/src/components/main/RenderImage.jsx
@@ -7,28 +7,45 @@ import MiraeAsset from '../../assets/ProductLogo/MiraeAsset.png';
 import KB from '../../assets/ProductLogo/KB.png';
 import KoreaInv from '../../assets/ProductLogo/KoreaInv.png';
 import Shinhan from '../../assets/ProductLogo/Shinhan.png';
-
+import NH from '../../assets/ProductLogo/NH.jpg';
+import Hi from '../../assets/ProductLogo/Hi.png';
+import Hana from '../../assets/ProductLogo/Hana.png';
 
 const imageMapping = {
-    'EUA': Europe,
-    'CKQ24': Europe,
-    'CKU24': Europe,
-    'CKV24': Europe,
-    'CKX24': Europe,
-    'CKZ24': Europe,
-    'CKH25': Europe,
-    'CKM25': Europe,
-    'SOL 유럽탄소배출권선물S&P(H)': Shinhan,
-    'KODEX 유럽탄소배출권선물ICE(H)': Samsung,
-    'SOL 유럽탄소배출권선물인버스ICE(H)': Shinhan,
-    'KraneShares European Carb Allw Stgy ETF': Kraneshares,
-    '메리츠 S&P 유럽탄소배출권 선물 ETN(H)': Meritz,
-    '미래에셋 S&P 유럽탄소배출권 선물 ETN': MiraeAsset,
-    'KB S&P 유럽탄소배출권 선물 ETN(H)': KB,
-    '한투 S&P 유럽탄소배출권 선물 ETN(H)': KoreaInv,
-    'KAU23': Korea,
-    'KAU24': Korea,
-    'KAU25': Korea
+  'EUA': Europe,
+  'CKQ24': Europe,
+  'CKU24': Europe,
+  'CKV24': Europe,
+  'CKX24': Europe,
+  'CKZ24': Europe,
+  'CKH25': Europe,
+  'CKM25': Europe,
+  'SOL 유럽탄소배출권선물S&P(H)': Shinhan,
+  'KODEX 유럽탄소배출권선물ICE(H)': Samsung,
+  'SOL 유럽탄소배출권선물인버스ICE(H)': Shinhan,
+  'KraneShares European Carb Allw Stgy ETF': Kraneshares,
+  '메리츠 S&P 유럽탄소배출권 선물 ETN(H)': Meritz,
+  '미래에셋 S&P 유럽탄소배출권 선물 ETN': MiraeAsset,
+  'KB S&P 유럽탄소배출권 선물 ETN(H)': KB,
+  '한투 S&P 유럽탄소배출권 선물 ETN(H)': KoreaInv,
+  'KAU23': Korea,
+  'KAU24': Korea,
+  'KAU25': Korea,
+  'SOL 국내 탄소배출권 ETN': Shinhan,
+  '미래에셋 국내 탄소배출권 ETN': MiraeAsset,
+  'NH 국내 탄소배출권 ETN': NH,
+  'KPS1230': Korea,
+  'KPS3134': Korea,
+  'KXC3213': Korea,
+  'KXC4123': Korea,
+  'KXC4125': Korea,
+  '하이투자증권 탄소 중립 ETF(EDS-4921)': Hi,
+  '미래에셋 국내탄소배출권 선물(FKE)': MiraeAsset,
+  'SOL 국내탄소배출권 선물(FKE)': Shinhan,
+  '한국투자증권 국내탄소배출권 선물(FKE)': KoreaInv,
+  '하나증권 NET Zero ETF(KETXE-444)': Hana,
+  '하나증권 NET Zero ETF(RESZ-KETS)': Hana,
+  '하이투자증권 탄소 중립 ETF(SHI-123z)': Hi,
 };
 
 export default imageMapping;
